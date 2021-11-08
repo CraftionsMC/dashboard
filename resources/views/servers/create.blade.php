70061c3595033001ed2cba2454cfaa3aec1a85bc
MCTzOCK <dev@mctzock.de>
2021-11-08 16:28:02 +0100

    [PATCH] Server Creation View

--- resources/views/servers/create.blade.php
+++ resources/views/servers/create.blade.php
@@ -33,119 +33,266 @@
                         <h5 class="card-title"><i class="fa fa-server mr-2"></i>Create Server</h5>
                     </div>
                     <div class="card-body">
-                        <form method="post" action="{{ route('servers.store') }}">
-                            @csrf
-                            <div class="form-group">
-                                <label for="name">* Name</label>
-                                <input id="name" name="name" type="text" required="required"
+
+                        <div class="form-group" id="step_0">
+                                <label for="name">Name</label>
+                                <input id="name" name="name" type="text" required="required" placeholder="Enter the name of your server"
                                     class="form-control @error('name') is-invalid @enderror">
+                        </div>
 
-                                @error('name')
-                                    <div class="invalid-feedback">
-                                        Please fill out this field.
-                                    </div>
-                                @enderror
+                        <div class="form-group" id="step_1" style="display: none">
+                            <label for="location_id">Server location</label>
+                            <div>
 
+                                <select id="node_id" name="node_id" required="required"
+                                    class="custom-select @error('node_id') is-invalid @enderror">
+                                    <option selected disabled hidden value="">Please Select ...</option>
+                                    @foreach ($locations as $location)
+                                        <optgroup label="{{ $location->name }}">
+                                            @foreach ($location->nodes as $node)
+                                                @if (!$node->disabled)
+                                                    <option value="{{ $node->id }}" selected>{{ $node->name }}</option>
+                                                @endif
+                                            @endforeach
+                                        </optgroup>
+                                    @endforeach
+
+                                </select>
                             </div>
-                            <div class="form-group">
-                                <label for="description">Description</label>
-                                <input id="description" name="description" type="text"
-                                    class="form-control @error('description') is-invalid @enderror">
 
-                                @error('description')
-                                    <div class="invalid-feedback">
-                                        Please fill out this field.
-                                    </div>
-                                @enderror
-
-                            </div>
-                            <div class="form-group">
-                                <label for="location_id">* Server location</label>
-                                <div>
-
-                                    <select id="node_id" name="node_id" required="required"
-                                        class="custom-select @error('node_id') is-invalid @enderror">
-                                        <option selected disabled hidden value="">Please Select ...</option>    
-                                        @foreach ($locations as $location)
-                                            <optgroup label="{{ $location->name }}">
-                                                @foreach ($location->nodes as $node)
-                                                    @if (!$node->disabled)
-                                                        <option value="{{ $node->id }}">{{ $node->name }}</option>
-                                                    @endif
-                                                @endforeach
-                                            </optgroup>
-                                        @endforeach
-
-                                    </select>
+                            @error('node_id')
+                                <div class="invalid-feedback">
+                                    Please fill out this field.
                                 </div>
-
-                                @error('node_id')
-                                    <div class="invalid-feedback">
-                                        Please fill out this field.
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="egg_id">* Server configuration</label>
-                                <div>
-                                    <select id="egg_id" name="egg_id" required="required"
-                                        class="custom-select @error('egg_id') is-invalid @enderror">
-                                       <option selected disabled hidden value="">Please Select ...</option>    
-                                        @foreach ($nests as $nest)
-                                            <optgroup label="{{ $nest->name }}">
-                                                @foreach ($nest->eggs as $egg)
-                                                    <option value="{{ $egg->id }}">{{ $egg->name }}</option>
-                                                @endforeach
-                                            </optgroup>
-                                        @endforeach
-                                    </select>
+                            @enderror
+                        </div>
+                        <div class="form-group" id="step_2" style="display: none">
+                            <h4 class="text-center">Select Service</h4>
+                            <div class="card" style="width: 18rem;">
+                                <img src="https://mctzock.de/static/media/minecraft.a9bb9ba9.png" class="card-img-top" width="128">
+                                <div class="card-body">
+                                    <h5 class="card-title">Minecraft</h5>
+                                    <p class="card-text">Host your own Minecraft Server in the Cloud.</p>
+                                    <a href="javascript:selectService(0)" class="btn btn-primary">Select</a>
                                 </div>
-
-                                @error('egg_id')
-                                    <div class="invalid-feedback">
-                                        Please fill out this field.
-                                    </div>
-                                @enderror
                             </div>
-                            <div class="form-group">
-                                <label for="product_id">* Resource Configuration</label>
-                                <div>
-                                    <select id="product_id" name="product_id" required="required"
-                                        class="custom-select @error('product_id') is-invalid @enderror">
-                                        <option selected disabled hidden value="">Please Select...</option>    
-                                        @foreach ($products as $product)
-                                            <option value="{{ $product->id }}" @if ($product->minimum_credits == -1 && Auth::user()->credits >= $minimum_credits)
-                                            @elseif ($product->minimum_credits != -1 && Auth::user()->credits >=
-                                                $product->minimum_credits)
-                                            @else
-                                                disabled
-                                        @endif
-                                        >{{ $product->name }}
-                                        ({{ $product->description }})
-                                        </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                @error('product_id')
-                                    <div class="invalid-feedback">
-                                        Please fill out this field.
-                                    </div>
-                                @enderror
+                        </div>
+                        <div class="form-group" id="step_3" style="display: none">
+                            <h4 class="text-center">Select Resources</h4>
+                            <div id="sel_res_cards">
                             </div>
-                            <div class="form-group text-right">
-                                <input type="submit" class="btn btn-primary mt-3" value="Submit"
-                                    onclick="this.disabled=true;this.value='Creating, please wait...';this.form.submit();">
+                        </div>
+                        <div class="form-group" id="step_4" style="display: none">
+                            <h4 class="text-center">Select Version</h4>
+                            <div id="sel_ver_cards">
                             </div>
-                        </form>
-
+                        </div>
+                        <div class="invalid-feedback" id="inv-feed" style="display: none">
+                            Please fill out this field.
+                        </div>
+                        <div class="form-group text-right" id="con_btn">
+                                <input type="button" class="btn btn-primary mt-3" value="Next"
+                                    onclick="nextStep();">
+                        </div>
                     </div>
                 </div>
+                <form id="h-f-sbm" style="display: none;" method="post" action="{{ route('servers.store') }}">
+                    @csrf
+                    <input id="form-name" name="name" type="text">
+                    <input id="form-description" name="description" type="text">
+                    <input id="form-node_id" name="node_id">
+                    <input id="form-egg_id" name="egg_id">
+                    <input id="form-product_id" name="product_id">
+                </form>
             </div>
             <!-- END CUSTOM CONTENT -->
 
 
         </div>
+        <script>
+
+            let currentStep = 0;
+            let currentService = 0;
+            let currentResource = 0;
+            let currentVersion = 0;
+
+            const services = [
+                "minecraft"
+            ]
+
+            const versions = {
+                minecraft: {
+                    vanilla: {
+                        display: "Vanilla",
+                        description: "Play Minecraft vanilla!",
+                        image: "https://mctzock.de/static/media/minecraft.a9bb9ba9.png"
+                    },
+                    paper: {
+                        display: "Paper",
+                        description: "Play Minecraft with plugins!",
+                        image: "https://avatars.githubusercontent.com/u/7608950?s=400&v=4"
+                    },
+                    forge: {
+                        display: "Forge",
+                        description: "Play Minecraft with mods!",
+                        image: "https://files.minecraftforge.net/static/images/logo.svg"
+                    }
+                }
+            }
+
+            const resources = {
+                minecraft: {
+                    small: {
+                        display: "Small",
+                        CPU: 50,
+                        RAM: 1024,
+                        Disk: 1024,
+                        Databases: 0,
+                        Backups: 1,
+                        prize: 25
+                    },
+                    medium: {
+                        display: "Medium",
+                        CPU: 100,
+                        RAM: 2048,
+                        Disk: 4096,
+                        Databases: 1,
+                        Backups: 4,
+                        prize: 55
+                    },
+                    large: {
+                        display: "Large",
+                        CPU: 150,
+                        RAM: 4096,
+                        Disk: 8192,
+                        Databases: 4,
+                        Backups: 8,
+                        prize: 100
+                    },
+                    mega: {
+                        display: "Mega",
+                        CPU: 200,
+                        RAM: 8192,
+                        Disk: 16384,
+                        Databases: 8,
+                        Backups: 20,
+                        prize: 187
+                    }
+                }
+            }
+
+            const elements = [
+                document.getElementById("name"),
+                document.getElementById("node_id"),
+                document.getElementById("egg_id"),
+                document.getElementById("product_id"),
+            ]
+
+            const ids = {
+                minecraft: {
+                    eggs: {
+                        vanilla: 5,
+                        paper: 3,
+                        forge: 2
+                    },
+                    products: {
+                        small: "Llwiwl1wpO9002UiLk-W3",
+                        medium: "tqKvbHRtGXGyeF3E3wpUh",
+                        large: "FTpULvx3qu0HslkWh4q8i",
+                        mega: "knaHw9wdh9psbgyRNIZ_d"
+                    }
+                }
+            }
+
+            function nextStep(force = false) {
+                document.getElementById("inv-feed").style.display = 'none'
+                document.getElementById("con_btn").style.display = 'block';
+                if(currentStep != 4){
+                    if(currentStep != 2 && currentStep != 3){
+                        if(!check(currentStep)){
+                            document.getElementById("inv-feed").style.display = 'block'
+                            return;
+                        }
+                    }
+                    currentStep++;
+                    document.getElementById("step_" + (currentStep - 1)).style.display = 'none';
+                    document.getElementById("step_" + currentStep).style.display = 'block';
+                }else {
+                    submit();
+                }
+                if(currentStep === 2 || currentStep === 3 || currentStep === 4){
+                    document.getElementById("con_btn").style.display = 'none';
+                }
+            }
+
+            function selectService(id) {
+                currentService = id;
+                nextStep();
+                if(currentService === 0){
+                    let s = ""
+                    Object.keys(resources.minecraft).forEach(x => {
+                        s += `
+                            <div class="card" style="width: 18rem; display: inline-block; margin: 1rem;">
+                                <img src="https://mctzock.de/static/media/minecraft.a9bb9ba9.png" class="card-img-top" width="128">
+                                <div class="card-body">
+                                    <h5 class="card-title">${resources.minecraft[x].display}</h5>
+                                    <ul class="card-text">
+                                        <li>CPU: ${resources.minecraft[x].CPU}%</li>
+                                        <li>RAM: ${resources.minecraft[x].RAM}MB</li>
+                                        <li>Disk: ${resources.minecraft[x].Disk}MB</li>
+                                        <li>Databases: ${resources.minecraft[x].Databases}</li>
+                                        <li>Backups: ${resources.minecraft[x].Backups}</li>
+                                        <li><b>${resources.minecraft[x].prize} Credits/Month</b></li>
+                                    </ul>
+                                    <a href="javascript:selectResource('${x}')" class="btn btn-primary">Select</a>
+                                </div>
+                            </div>
+                        `
+                    })
+                    document.getElementById("sel_res_cards").innerHTML = s;
+                }
+            }
+
+            function selectResource(name) {
+                currentResource = name;
+                nextStep();
+                if(currentService === 0){
+                    let s = ""
+                    Object.keys(versions.minecraft).forEach(x => {
+                        s += `
+                            <div class="card" style="width: 18rem; display: inline-block; margin: 1rem;">
+                                <img src="${versions.minecraft[x].image}" class="card-img-top" width="128" height="256">
+                                <div class="card-body">
+                                    <h5 class="card-title">${versions.minecraft[x].display}</h5>
+                                    <p class="card-text">
+                                        ${versions.minecraft[x].description}
+                                    </p>
+                                    <a href="javascript:selectVersion('${x}')" class="btn btn-primary">Select</a>
+                                </div>
+                            </div>
+                        `
+                    })
+                    document.getElementById("sel_ver_cards").innerHTML = s;
+                }
+            }
+
+            function selectVersion(version) {
+                document.getElementById("form-name").value = document.getElementById("name").value;
+                document.getElementById("form-description").value = "";
+                document.getElementById("form-node_id").value = document.getElementById("node_id").value;
+                document.getElementById("form-egg_id").value = ids.minecraft.eggs[version];
+                document.getElementById("form-product_id").value = ids.minecraft.products[currentResource];
+                submit();
+            }
+
+            function check(step) {
+                return elements[step].value != "" && elements[step].value != null;
+            }
+
+            function submit() {
+                document.getElementById("h-f-sbm").submit();
+            }
+        </script>
     </section>
     <!-- END CONTENT -->
 
