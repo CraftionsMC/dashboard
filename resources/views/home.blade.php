36f8c0f32c45276f9deea4c8bb256de5b9de5e68
MCTzOCK <dev@mctzock.de>
2021-11-08 17:15:51 +0100

    [PATCH] Icon Background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,7 @@
                 @endif
                 <div class="col-12 col-sm-6 col-md-3" style="cursor: pointer" onClick="window.location.assign('/store')">
                     <div class="info-box mb-3">
-                        <span class="info-box-icon {{$bg}} elevation-1">
+                        <span class="info-box-icon bg-success elevation-1">
                             <i class="fas fa-plus-square"></i>
                             </span>
                         <div class="info-box-content">
